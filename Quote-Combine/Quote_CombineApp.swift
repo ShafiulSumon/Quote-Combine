cff4118cb8dadf94e3f90eb8366e0c7f9e0b9ace
Md_Shafiul_Alam <shafiul.alam@bjitgroup.com>
2023-04-13 09:34:26 +0600

    Initial Commit

---
+++ Quote-Combine/Quote_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quote_CombineApp.swift
+//  Quote-Combine
+//
+//  Created by ShafiulAlam-00058 on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Quote_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
